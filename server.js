240a7d687126e97775cd6a9cc599ae7cfd0336fb
Nancy <nancyrosas0@gmail.com>
2021-04-30 15:44:46 -0500

    added route and query

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,24 @@ const app = express();
 app.use(express.urlencoded({ extend: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Oranges25!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// This is the catchall route
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
