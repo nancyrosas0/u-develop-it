726941333d43dc6ad5d20277618831358b3303f1
Nancy <nancyrosas0@gmail.com>
2021-05-01 17:35:15 -0500

    added voter routes

--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection');
 
 // GET route for all parties - API
-router.get('/api/parties', (req, res) => {
+router.get('/parties', (req, res) => {
     const sql = 'SELECT * FROM parties';
     db.query(sql, (err, rows) => {
         if (err) {
@@ -18,7 +18,7 @@ router.get('/api/parties', (req, res) => {
 });
 
 // GET route for SINGLE party - API
-router.get('/api/party/:id', (req, res) => {
+router.get('/party/:id', (req, res) => {
     const sql = `SELECT * FROM parties WHERE id = ?`;
     const params = [req.params.id];
     db.query(sql, params, (err, row) => {
@@ -35,7 +35,7 @@ router.get('/api/party/:id', (req, res) => {
 
   
 // Delete a party
-router.delete('/api/party/:id', (req, res) => {
+router.delete('/party/:id', (req, res) => {
     const sql = 'DELETE FROM parties WHERE id =?';
     const params = [req.params.id];
     db.query(sql, params, (err, result) => {
